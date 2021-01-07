fe8f6dad3f0d0d4d9b0d3a26b948ec20531e4f1f
colinChen <709729345@qq.com>
2021-01-07 15:24:13 +0800

    update

--- config.js
+++ config.js
@@ -3,7 +3,7 @@ window.spiderSlaveApi = "http://test.blog.com/";
 window.spiderSlaveDelay = 5000;
 window.spiderSlaveGetUrlsDelay = 5000;
 window.spiderSlaveTabCount = 2;
-window.spiderSlaveOff = false;
+window.spiderSlaveOff = true;
 window.spiderSlaveDebug = false;
 
 window.baseInfo = {};
@@ -26,20 +26,33 @@ initDeviceInfo(function(){
 });
 
 function initDeviceInfo(cb) {
-	chrome.system.display.getInfo(function(info) {
-		chrome.windows.getCurrent(function(win) {
-			window.baseInfo['windowId'] = win.id;
-			window.baseInfo['height'] = info[0].bounds.height;
-			window.baseInfo['width'] = info[0].bounds.width;
-			
-			window.baseInfo['xCount'] = Math.ceil(Math.sqrt(window.spiderSlaveTabCount+1));
-			window.baseInfo['yCount'] = Math.ceil((window.spiderSlaveTabCount+1)/window.baseInfo['xCount']);
-			window.baseInfo['perHeight'] = parseInt(window.baseInfo['height']/window.baseInfo['yCount']);
-			window.baseInfo['perWidth'] = parseInt(window.baseInfo['width']/window.baseInfo['xCount']);
-			
-			chrome.windows.update(window.baseInfo['windowId'],{state:'normal',top:0,left:0,height:window.baseInfo['perHeight'],width:window.baseInfo['perWidth']},function(newWin) {
-				cb && cb();
+	loadConfig(function() {
+		chrome.system.display.getInfo(function(info) {
+			chrome.windows.getCurrent(function(win) {
+				window.baseInfo['windowId'] = win.id;
+				window.baseInfo['height'] = info[0].bounds.height;
+				window.baseInfo['width'] = info[0].bounds.width;
+				
+				window.spiderSlaveTabCount = parseInt(window.spiderSlaveTabCount);
+				window.baseInfo['xCount'] = Math.ceil(Math.sqrt(window.spiderSlaveTabCount+1));
+				window.baseInfo['yCount'] = Math.ceil((window.spiderSlaveTabCount+1)/window.baseInfo['xCount']);
+				window.baseInfo['perHeight'] = parseInt(window.baseInfo['height']/window.baseInfo['yCount']);
+				window.baseInfo['perWidth'] = parseInt(window.baseInfo['width']/window.baseInfo['xCount']);
+				
+				chrome.windows.update(window.baseInfo['windowId'],{state:'normal',top:0,left:0,height:window.baseInfo['perHeight'],width:window.baseInfo['perWidth']},function(newWin) {
+					cb && cb();
+				});
 			});
 		});
+	});
+}
+
+function loadConfig(cb) {
+	chrome.storage.local.get(null, function(result) {
+		for(var key in result) {
+			window[key] = result[key];
+		}
+		
+		cb && cb();
 	});
 }
--- popup/js/popup.js
+++ popup/js/popup.js
@@ -6,7 +6,10 @@ layui.use(['element', 'layer', 'form', 'jquery'], function(){
 	var bg = chrome.extension.getBackgroundPage();
 	form.val('workerConfig', {
       "spiderSlaveFlag": bg.spiderSlaveFlag
+      ,"spiderSlaveApi": bg.spiderSlaveApi
       ,"spiderSlaveTabCount": bg.spiderSlaveTabCount
+      ,"spiderSlaveOff": bg.spiderSlaveOff
+      ,"spiderSlaveDebug": bg.spiderSlaveDebug
     });
 	
 	$('#worker_status').click(function() {
@@ -19,8 +22,14 @@ layui.use(['element', 'layer', 'form', 'jquery'], function(){
 		}
 	});
 	
-	$('#worker_refresh').click(function() {
+	//监听提交
+	form.on('submit(workerConfig)', function(data){
+		chrome.storage.local.set(data.field,function(){
+			layer.msg('储存成功');
+		});
 		
+		bg.loadConfig();
+	    return false;
 	});
 	//监听折叠
 //	element.on('collapse(main)', function(data){
