c936a8024f0749bb95fadbb51897a7da550fd815
colinChen <709729345@qq.com>
2025-09-28 16:39:34 +0800

    Update start.js

--- background/start.js
+++ background/start.js
@@ -14,17 +14,18 @@ importScripts(
 );
 
 //eval.js 发生改变直接reload
-var evalMd5 = undefined;
+var evalLastModified = undefined;
 setInterval(()=>{
   const url = chrome.runtime.getURL('eval.js') + '?t=' + Date.now();
-  fetch(url).then((response)=>{
-    return response.text()
-  }).then(async (content)=>{
-    var md5 = SparkMD5.hash(content);
-    if(evalMd5 === undefined) {
-      evalMd5 = md5;
+  fetch(url,{
+      method: 'HEAD',
+  }).then((response)=>{
+    return response.headers.get('last-modified')
+  }).then(async (lastmodified)=>{
+    if(evalLastModified === undefined) {
+      evalLastModified = lastmodified;
       return ;
-    }else if(evalMd5 != md5) {
+    }else if(evalLastModified != lastmodified) {
       await chrome.storage.local.set({'spiderSlaveUrls':window.spiderSlaveUrls});
 		  await chrome.storage.local.set({'spiderSlaveTabInfos':window.spiderSlaveTabInfos});
       chrome.runtime.reload();
