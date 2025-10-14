5bebc61a56b27409778db5eafe73561f40c351a2
colinChen <709729345@qq.com>
2025-10-14 11:43:43 +0800

    Update selector.js

--- content/selector.js
+++ content/selector.js
@@ -47,30 +47,90 @@ function getElementSelector(element) {
 
 function checkContentIncludeText(textContent, texts) {
   textContent = textContent.toLowerCase();
-  
 
   if(typeof texts === 'string') {
     texts = [texts];
   }
 
   var allMatch = true;
+  var score = 0;
+  var subMatchIndex = [];
+  var keyOne = 0;
   texts.forEach(text => {
-    text = text.toLowerCase();
-    if (!textContent.includes(text)) {
+    var textOrg = text;
+    var include = true;
+    if(isObject(textOrg)) {
+      if(textOrg['type'] && textOrg['type'] === '|') {
+        include = false;
+      }
+
+      if(textOrg['type'] && textOrg['type'] === 'maxlength') {
+        if(textContent.length > textOrg['text']) {
+          allMatch = false;
+        }
+        return ;
+      }
+      text = textOrg['text'];
+    }
+
+    if(typeof text === 'string') {
+      text = [text];
+    }
+
+    var subMatch = false;
+    var keyTwo = 0;
+    text.forEach(subTextOrg => {
+      subText = subTextOrg.toLowerCase();
+      var idx = textContent.indexOf(subText);
+      if (idx > -1) {
+        subMatch = true;
+        subMatchIndex.push({text:subTextOrg,keys:[keyOne,keyTwo],index:idx,nodeText:"..."+textContent.substr(idx-20<0?0:idx-20,subTextOrg.length+40)+"..."});
+      }
+
+      keyTwo++;
+    });
+
+    if(include === true && subMatch === false) {
       allMatch = false;
     }
+
+    if(subMatch) {
+      score += 1;
+    }
+
+    keyOne++;
   });
-  return allMatch;
+  
+  return [allMatch,score,subMatchIndex];
+}
+
+function isArray(value) {
+    return Object.prototype.toString.call(value) === '[object Array]';
+}
+
+function isObject(value) {
+  return value !== null && typeof value === 'object' && Object.prototype.toString.call(value) === '[object Object]';
 }
 
 function getElementByText(text) {
-  var allMatches = Array.from(document.querySelectorAll("*")).filter((el) =>
-    checkContentIncludeText(el.textContent,text)
+  var allMatches = Array.from(document.querySelectorAll("*")).filter((el) => {
+    var checkInfo = checkContentIncludeText(el.textContent,text);
+    if(checkInfo[0]) {
+      el.checkInfo = checkInfo;
+    }
+    return checkInfo[0];
+  }
   );
 
   var deepestMatches = allMatches.filter((el) => {
-    var hasMatchingChild = Array.from(el.querySelectorAll("*")).some((child) =>
-      checkContentIncludeText(child.textContent,text)
+    var hasMatchingChild = Array.from(el.querySelectorAll("*")).some((child) => {
+      var checkInfo = checkContentIncludeText(child.textContent,text);
+      if(checkInfo[0]) {
+        child.checkInfo = checkInfo;
+      }
+
+      return checkInfo[0];
+    }
     );
     return !hasMatchingChild;
   });
@@ -127,7 +187,7 @@ function getBetchSelectorByTexts(texts) {
     var selectors = [];
     deepestMatches.forEach(deepestMatche => {
       var findSelector = getElementSelector(deepestMatche);
-      selectors.push([deepestMatche.textContent,findSelector,getElementPosition(deepestMatche)]);
+      selectors.push([deepestMatche.textContent,findSelector,deepestMatche.checkInfo[1],deepestMatche.checkInfo[2],getElementPosition(deepestMatche)]);
     });
     r[field] = selectors;
   }
@@ -137,9 +197,9 @@ function getBetchSelectorByTexts(texts) {
 
 //test
 
-/* 
-setTimeout(() => {
 
+setTimeout(() => {
+/* 
   //选择器测试
   var selectorTemps = [
     "#main-container > div > div > div.main-list_container > div.title_all > span.folder-title-style > span.mode_icon.list_mode.list_mode_checked",
@@ -165,11 +225,18 @@ setTimeout(() => {
     var findSelector = getElementSelector(deepestMatche);
     console.log(targetText,'=>',getElementPosition(deepestMatche),findSelector,deepestMatche);
   }); 
+  */
 
   //https://www.oschina.net/news/375428
   var texts = {
     title: "宇树被诉侵害发明专利权一案一审宣判：不构成侵权",
-    date: "2025-09-30 14:04:26",
+    // date: "2025-09-30 14:04:26",
+    date: [
+      { type: "|", text: ["2025", "25"] },
+      { type: "&", text: ["09", "9"] },
+      { type: "&", text: ["30"] },
+      { type: "maxlength", text: 100 },
+    ],
     author: "白开水不加糖",
     content: [
       "宇树科技被杭州露韦美日化有限公司诉侵害发明专利权一案，已于本月 26 日宣判，宇树科技不构成侵权。原告败诉，法院已驳回原告全部诉讼请求。",
@@ -178,4 +245,4 @@ setTimeout(() => {
   };
 
   console.log(getBetchSelectorByTexts(texts))
-}, 5000);  */
+}, 5000);  
